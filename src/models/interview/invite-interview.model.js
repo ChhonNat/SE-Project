32489485cd6c22d04394a45163a070d9bd835eb6
longmadeth <madeth.long@mjqeducation.edu.kh>
2023-08-01 17:30:54 +0700

    set require for hour and time in invite interview

--- src/models/interview/invite-interview.model.js
+++ src/models/interview/invite-interview.model.js
@@ -1,13 +1,13 @@
 import { number, object, string } from "zod";
 
 const InviteInterviewModel = object({
-    interviewDate: string().min(1, { message: 'Schedule date is required!' }),
+    interviewDate: string().min(1, { message: 'Date is required!' }),
     headDepartmentId: number().min(1, { message : 'Head of department is required!' }),
     committees: number().array().min(0).default([]),
     remark: string().optional().nullable(),
 
-    hour:string().optional().nullable(),
-    min:string().optional().nullable(),
+    hour:string().min(1,{message: 'Hour is required!'}),
+    min:string().min(1, {message: 'Minute is required!'}),
     meridiem:string().optional().nullable()
 });
 
