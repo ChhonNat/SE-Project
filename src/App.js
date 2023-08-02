6d87d7ce3e832e7b06fcb07a872b9d5eadb460e1
longmadeth <madeth.long@mjqeducation.edu.kh>
2023-08-02 14:35:39 +0700

    fixed when login with incorrect user losed user from input

--- src/App.js
+++ src/App.js
@@ -42,132 +42,96 @@ const App = () => {
   /**
    * Loading while while route fallback
    */
-
   const Loading = () => {
     return <p className="global-loading">Loading...</p>
   };
 
-  /**
-   * Page not found status
-   */
-  const PageNotFound = () => {
-    return <p className="page-not-found">Page not found!</p>
-  }
-
-  /**
-   * Private routes with private elements
-   */
-  const PrivateRouters = () => {
-    return (
-      <>
-        {/* Navbar component */}
-        <Navbar />
-
-        {/* Sidebar component */}
-        <Sidebar />
-        {/* All routes */}
-        <Suspense fallback={<Loading />}>
-
-          <Routes>
-            {
-              PRIVATE_ROUTES && PRIVATE_ROUTES.length && PRIVATE_ROUTES.map((route, index) => (
-
-                < React.Fragment key={index} >
-
-                  {/* Parent routes */}
-                  <Route path={route?.path}
-                    element={
-                      <MainPageComponent
-                        outlet={route.component}
-                      />
-                    }
-                    key={index} />
-
-                  {/* Child routes */}
-                  {route?.children?.length && route?.children.map((childRoute, childIndex) => (
-
-                    <Route path={route?.path} key={index}>
-                      {/* Child route components */}
-                      <Route
-                        path={childRoute?.path}
-                        element={
-                          <MainPageComponent
-                            // open={minSidebar} 
-                            outlet={childRoute.component}
-                          />
-                        }
-                        key={childIndex}
-                      />
-
-                      {/* Child route child route components */}
-                      {
-                        childRoute?.children?.length && childRoute?.children?.map((childChildRoute, childChildRouteIndex) => (
-
-                          <Route path={route?.path + childRoute?.path} key={childChildRouteIndex}>
-                            <Route path={childChildRoute?.path}
-                              element={
-                                <MainPageComponent
-                                  outlet={childChildRoute.component}
-                                />
-                              }
-                              key={childChildRouteIndex}
-                            />
-                          </Route>
-                        ))
-                      }
-                    </Route>
-                  ))}
-                </React.Fragment>
-
-              ))
-            }
-          </Routes>
-
-        </Suspense >
-      </>
-    );
-  };
-
-
-  /**
-   * Public routes with public elements
-   */
-  const PublicRouters = () => {
-    return (
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          {
-            PUBLIC_ROUTES && PUBLIC_ROUTES?.length && PUBLIC_ROUTES.map((route, index) => (
-              <React.Fragment key={index}>
-                <Route path={route?.path} element={route?.component} />
-              </React.Fragment>
-            ))
-          }
-        </Routes>
-      </Suspense>
-    );
-  };
-
-
-
-  /**
-   * Switch screen between authorize and unauthorize user
-   */
-  const Screen = () => {
-    return (
-      <>
-        {user?.isAuthenticated ? <PrivateRouters /> : <PublicRouters />}
-
-      </>
-    );
-  };
 
   /**
    * return private router if user has authentication, public router if user unauthentication
    */
   return (
     <>
-      <Screen />
+      {
+        user?.isAuthenticated ?
+          <>
+            {/* Navbar component */}
+            <Navbar />
+
+            {/* Sidebar component */}
+            <Sidebar />
+            {/* All routes */}
+            <Suspense fallback={<Loading />}>
+
+              <Routes>
+                {
+                  PRIVATE_ROUTES && PRIVATE_ROUTES.length && PRIVATE_ROUTES.map((route, index) => (
+
+                    < React.Fragment key={index} >
+
+                      {/* Parent routes */}
+                      <Route path={route?.path}
+                        element={
+                          <MainPageComponent
+                            outlet={route.component}
+                          />
+                        }
+                        key={index} />
+
+                      {/* Child routes */}
+                      {route?.children?.length && route?.children.map((childRoute, childIndex) => (
+
+                        <Route path={route?.path} key={index}>
+                          {/* Child route components */}
+                          <Route
+                            path={childRoute?.path}
+                            element={
+                              <MainPageComponent
+                                outlet={childRoute.component}
+                              />
+                            }
+                            key={childIndex}
+                          />
+
+                          {/* Child route child route components */}
+                          {
+                            childRoute?.children?.length && childRoute?.children?.map((childChildRoute, childChildRouteIndex) => (
+
+                              <Route path={route?.path + childRoute?.path} key={childChildRouteIndex}>
+                                <Route path={childChildRoute?.path}
+                                  element={
+                                    <MainPageComponent
+                                      outlet={childChildRoute.component}
+                                    />
+                                  }
+                                  key={childChildRouteIndex}
+                                />
+                              </Route>
+                            ))
+                          }
+                        </Route>
+                      ))}
+                    </React.Fragment>
+
+                  ))
+                }
+              </Routes>
+
+            </Suspense >
+          </> 
+          :
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              {
+                PUBLIC_ROUTES && PUBLIC_ROUTES?.length && PUBLIC_ROUTES.map((route, index) => (
+                  <React.Fragment key={index}>
+                    <Route path={route?.path} element={route?.component} />
+                  </React.Fragment>
+                ))
+              }
+            </Routes>
+          </Suspense>
+      }
     </>
   );
 };
