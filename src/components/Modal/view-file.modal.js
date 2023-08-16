03a5fad74a15064a4359d4a4bcc4ff5f86622cf2
longmadeth <madeth.long@mjqeducation.edu.kh>
2023-08-16 17:15:09 +0700

    add authorize request header in view file

--- src/components/Modal/view-file.modal.js
+++ src/components/Modal/view-file.modal.js
@@ -1,75 +1,100 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 import TitleComponent from "../Page/title";
 import FooterComponent from "../Page/footer";
 import apiLink from "../../constants/app_cont";
-import { Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Slide } from "@mui/material";
-import { Close } from "@mui/icons-material";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Slide,
+} from "@mui/material";
+import { Block, Close } from "@mui/icons-material";
 import { API_URL } from "../../constants/api_url";
+import axiosAPI from "../../services/http.service";
+import { HTTP_STATUS } from "../../constants/http_status";
 
 const TransitionModal = forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
+  return <Slide direction="up" ref={ref} {...props} />;
 });
 
-
 const ViewFileModal = (props) => {
+  const { openModal, onCloseModal, id, modalTitle, downloadFileUrl } = props;
 
-    const { openModal, onCloseModal, id, modalTitle, downloadFileUrl  } = props;
+  // const url = downloadFileUrl ? `${apiLink}${downloadFileUrl}${id}` : `${apiLink}${API_URL.candidate.downloadCVFile}${id}`;
+  const url = downloadFileUrl
+    ? `${downloadFileUrl}${id}`
+    : `${API_URL.candidate.downloadCVFile}${id}`;
+  const [displayPdf, setDisplayPdf] = useState("");
 
-    const url = downloadFileUrl ? `${apiLink}${downloadFileUrl}${id}` : `${apiLink}${API_URL.candidate.downloadCVFile}${id}`
+  useEffect(() => {
+    getFileToDisplay();
+  }, []);
 
-    return (
-        <>
-            <Dialog
-                fullScreen={true}
-                TransitionComponent={TransitionModal}
-                open={openModal}
-                sx={{ margin: 1 }}
-                onClose={onCloseModal}
+  const getFileToDisplay = async () => {
+    try {
+      const reqFile = await axiosAPI.get(url, { responseType: "arraybuffer" });
+      const { status } = reqFile;
+
+      if (status === HTTP_STATUS.success) {
+        const { data } = reqFile;
+        const blob = new Blob([data], { type: "application/pdf" });
+        const blobToPdf = window.URL.createObjectURL(blob) + "#view=FitW";
+        setDisplayPdf(blobToPdf);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Dialog
+        fullScreen={true}
+        TransitionComponent={TransitionModal}
+        open={openModal}
+        sx={{ margin: 1 }}
+        onClose={onCloseModal}
+      >
+        <DialogTitle>
+          <TitleComponent title={modalTitle} />
+          {onCloseModal ? (
+            <IconButton
+              aria-label="close"
+              onClick={onCloseModal}
+              sx={{
+                position: "absolute",
+                right: 8,
+                top: 8,
+                color: (theme) => theme.palette.grey[500],
+              }}
             >
-                <DialogTitle>
-                    <TitleComponent title={modalTitle} />
-                    {
-                        onCloseModal ? (
-                            <IconButton
-                                aria-label="close"
-                                onClick={onCloseModal}
-                                sx={{
-                                    position: 'absolute',
-                                    right: 8,
-                                    top: 8,
-                                    color: (theme) => theme.palette.grey[500],
-                                }}
-                            >
-                                <Close />
-                            </IconButton>
-                        ):
-                        null
-                    }
-                </DialogTitle>
+              <Close />
+            </IconButton>
+          ) : null}
+        </DialogTitle>
 
-                <DialogContent dividers>
-
-                    <embed
-                        src={url}
-                        style={
-                            {
-                                webkitWidth: '-webkit-fill-available',
-                                height: '100%',
-                                width: '100%'
-                            }}
-                    />
-
-                </DialogContent>
-                <DialogActions>
-                    <FooterComponent
-                        handleCancel={onCloseModal}
-                        actions={{ cancel: true, submit: false }}
-                        cancelButtonLabel="Close"
-                    />
-                </DialogActions>
-            </Dialog>
-        </>
-    )
+        <DialogContent dividers>
+          <embed
+            src={displayPdf}
+            style={{
+              webkitWidth: "-webkit-fill-available",
+              height: "100%",
+              width: "100%",
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <FooterComponent
+            handleCancel={onCloseModal}
+            actions={{ cancel: true, submit: false }}
+            cancelButtonLabel="Close"
+          />
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 };
 
 export default ViewFileModal;
