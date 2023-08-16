b5ea0e6fec628d2801a43bf32ef7b6dc0e688100
longmadeth <madeth.long@mjqeducation.edu.kh>
2023-08-16 15:55:56 +0700

    change to UAT test

--- src/constants/app_cont.js
+++ src/constants/app_cont.js
@@ -1,4 +1,9 @@
-const apiLink = "http://172.168.0.38:8585";
+//DEVELOPMENT
+// const apiLink = "http://172.168.0.38:8585";
+
+//UTA
+const apiLink = "http://172.168.0.38:8081";
+
 // const apiLink = "http://localhost:8080";
 
 export default apiLink;
